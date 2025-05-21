ca0408f184b639cab4729f129e7cbd918fb2c9e9
tsayush2390 <115918401+tsayush2390@users.noreply.github.com>
2025-05-21 21:43:42 +0530

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -22,21 +22,49 @@ const Cryptocurrencies = ({simplified}) => {
     {!simplified && (<div className='search-crypto'>
       <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
     </div> )}
-    <Row gutter={[32, 32]} className='crypto-card-container'>
-      {cryptos?.map((currency) => (
-        <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
-          <Link to={`/crypto/${currency.uuid}`}>
-            <Card title={`${currency.rank}. ${currency.name}`}
-            extra={<img className='crypto-image' src={currency.iconUrl} />}
-            hoverable >
-              <p>Price: {millify(currency.price)}</p>
-              <p>Market Cap: {millify(currency.marketCap)}</p>
-              <p>daily Change: {millify(currency.change)}</p>
-            </Card>
-          </Link>
-        </Col> 
+   {viewMode === 'card' ? (
+  <Row gutter={[32, 32]} className='crypto-card-container'>
+    {sortedCryptos?.map((currency) => (
+      <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+        <Link to={`/crypto/${currency.uuid}`}>
+          <Card title={`${currency.rank}. ${currency.name}`} extra={<img className='crypto-image' src={currency.iconUrl} />} hoverable>
+            <p>Price: {millify(currency.price)}</p>
+            <p>Market Cap: {millify(currency.marketCap)}</p>
+            <p>Daily Change: {millify(currency.change)}</p>
+          </Card>
+        </Link>
+      </Col>
+    ))}
+  </Row>
+) : (
+  <table style={{ width: '100%', marginTop: '1rem' }}>
+    <thead>
+      <tr>
+        <th>Rank</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Market Cap</th>
+        <th>Daily Change</th>
+      </tr>
+    </thead>
+    <tbody>
+      {sortedCryptos?.map((currency) => (
+        <tr key={currency.id}>
+          <td>{currency.rank}</td>
+          <td>
+            <Link to={`/crypto/${currency.uuid}`}>
+              <img src={currency.iconUrl} style={{ width: 20, marginRight: 8 }} />
+              {currency.name}
+            </Link>
+          </td>
+          <td>{millify(currency.price)}</td>
+          <td>{millify(currency.marketCap)}</td>
+          <td>{millify(currency.change)}</td>
+        </tr>
       ))}
-    </Row>
+    </tbody>
+  </table>
+)}
     </>
   )
 }
